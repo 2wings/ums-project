fd1a09a704931ea904e8c8ac2d8a624776ee75e5
John Lau <liushuangyi@126.com>
2014-05-20 15:34:03 +0800

    add user login event

--- ums-reactor-sample/ums-reactor-core/src/main/java/ums/reactor/event/UserEvent.java
+++ ums-reactor-sample/ums-reactor-core/src/main/java/ums/reactor/event/UserEvent.java
@@ -26,6 +26,10 @@ public interface UserEvent {
     String USER_GET_ALL = "user.getall";
     
     String USER_QUERY_REPLY = "user.queryall.reply";
+
+    String USER_LOGIN = "user.login";
+
+    String USER_LOGIN_REPLY = "user.login.reply";
     
 
 }
