a3fe77e0a2292f90e4297174231f8cc595841184
John Lau <liushuangyi@126.com>
2014-05-20 15:34:03 +0800

    add user login event

--- root/ums-reactor-sample/ums-reactor-core/src/main/java/ums/reactor/handler/UserHandler.java
+++ root/ums-reactor-sample/ums-reactor-core/src/main/java/ums/reactor/handler/UserHandler.java
@@ -1,6 +1,9 @@
 package ums.reactor.handler;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.ejb.EJB;
 
@@ -9,7 +12,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import reactor.core.Reactor;
+import reactor.core.composable.Stream;
+import reactor.core.composable.spec.Streams;
 import reactor.event.Event;
+import reactor.function.Function;
 import reactor.spring.annotation.Consumer;
 import reactor.spring.annotation.ReplyTo;
 import reactor.spring.annotation.Selector;
@@ -48,5 +54,17 @@ public class UserHandler {
     public List<User> handleQueryAll(Event evt) {
         return userDaoBean.readAll();
     }
-    
+
+    @Selector(value = UserEvent.USER_LOGIN, reactor = "@rootReactor")
+    @ReplyTo(UserEvent.USER_LOGIN_REPLY)
+    public boolean login(Event evt) {
+        User user = (User) evt.getData();
+        User existedUser = userDaoBean.find(user.getUserName());
+        if (existedUser != null && user.getPassword().equals(existedUser.getPassword())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
