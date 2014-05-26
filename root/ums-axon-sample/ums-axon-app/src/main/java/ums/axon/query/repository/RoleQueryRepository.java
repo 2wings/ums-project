bcb99a53f0bd9b75ba11cb8629edb1d704196090
John Lau <liushuangyi@126.com>
2014-05-26 15:12:15 +0800

    remove root

--- root/ums-axon-sample/ums-axon-app/src/main/java/ums/axon/query/repository/RoleQueryRepository.java
+++
@@ -1,8 +0,0 @@
-package ums.axon.query.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import ums.axon.query.RoleEntry;
-
-public interface RoleQueryRepository extends MongoRepository<RoleEntry, String> {
-}
